c42f8452cfde07b3487a2e1aeabc508f99537f30
tenten09 <y.koh3103@gmail.com>
2021-03-28 17:22:34 +0900

    デプロイのため一部UI修正

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,19 +1,30 @@
 <template>
   <div>
     <b-container>
-      <b-row :key="index" v-for="(todo, index) in todos" class="mt-2">
-        <b-col cols="8" :class="[{line: todo.complete}, 'text-left']">
-          <h5>{{todo.text}}</h5>
-        </b-col>
-        <b-col cols="4" class="text-right">
-          <b-button 
-            @click="handleCompleteTodo(index)" 
-            :variant="todo.complete ? '' : 'success'">
-            {{todo.complete ? '完了' : '未完了'}}
-          </b-button>
-          <b-button @click="handleDeleteTodo(index)" variant="danger">delete</b-button>
-        </b-col>
-      </b-row>
+      <b-card
+        title="TODO"
+        img-src="https://picsum.photos/600/300/?image=25"
+        img-alt="Image"
+        img-top
+        tag="article"
+        style="height: 20;"
+        class="mb-2" 
+        img-height="240"
+     >
+        <b-row :key="index" v-for="(todo, index) in todos" class="mt-2">
+          <b-col cols="8" :class="[{line: todo.complete}, 'text-left']">
+            <h5>{{todo.text}}</h5>
+          </b-col>
+          <b-col cols="4" class="text-right">
+            <b-button 
+              @click="handleCompleteTodo(index)" 
+              :variant="todo.complete ? '' : 'primary'">
+              {{todo.complete ? '完了' : '未完了'}}
+            </b-button>
+            <b-button @click="handleDeleteTodo(index)" variant="danger">delete</b-button>
+          </b-col>
+        </b-row>
+      </b-card>
     </b-container>
   </div>
 </template>
